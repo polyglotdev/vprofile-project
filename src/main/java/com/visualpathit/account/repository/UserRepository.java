5dbb5b8d9ea2a641a2b85edaf9cd63ad87a18a04
Dom Hallan <dom@domhallan.com>
2024-06-05 10:28:53 -0400

    chore: Update trufflehog npm dependency to version 3.78.0

--- src/main/java/com/visualpathit/account/repository/UserRepository.java
+++ src/main/java/com/visualpathit/account/repository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findById(long id);
     /*public void updateUser(User user)*/;
-    
+
 }
